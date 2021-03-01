318a45f89f99a48029ee830247de502502124022
Traveler2806 <dannyvkt88@gmail.com>
2021-03-01 14:26:18 -0600

    get playerName

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,8 +4,20 @@ var randomNumber = function(min, max) {
     return value;
 }
 
+var getPlayerName = function() {
+    var name = "";
+    
+    while(name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+    }
+    {
+    console.log("Your robot's name is" + name);
+    return name;
+    }
+};
+
 var playerInfo = {
-    name: window.prompt("What is your robot's name"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
@@ -62,7 +74,7 @@ var startGame = function() {
         if (playerInfo.health > 0) {
             // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
             window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        
+
             // pick new enemy to fight based on the index of the enemy.names array
             var pickedEnemyObj = enemyInfo[i];
         
